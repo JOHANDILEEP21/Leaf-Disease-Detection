a7b7a4bdc3ff54999e02dfe8632cd025323d8174
DILEEPKUMAR <110006271+JOHANDILEEP21@users.noreply.github.com>
2024-08-01 17:47:23 +0530

    Update models.py

--- models.py
+++ models.py
@@ -31,7 +31,7 @@ class leaf_disease_detection:
     #path=input("Enter your image path-: ")
     def testing(path):
         
-        ldd_model = pickle.load(open('https://github.com/JOHANDILEEP21/Leaf-Disease-Detection/blob/1f6c080d0374784f4e33540a3c27aed0836028fa/Leaf_disease_detection.pkl', 'rb'))
+        ldd_model = pickle.load(open(''https://github.com/JOHANDILEEP21/Leaf-Disease-Detection/blob/main/Leaf_disease_detection.pkl', 'rb'))
 
         # ldd_model.summary()
 
